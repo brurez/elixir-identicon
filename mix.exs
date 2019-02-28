b62578ac02ef4bc13d1754e9a3186d11a05f2840
brurez <brunoderezende@hotmail.com>
2019-02-28 19:59:22 -0300

    Builds the grid

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule ElixirIdenticon.MixProject do
+defmodule Identicon.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :elixir_identicon,
+      app: :identicon,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
